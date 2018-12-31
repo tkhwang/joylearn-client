7d8a4cfaca841f46b4ee8c2f7b06c6f54b7060ee
tkhwang <tkhwang@gmail.com>
2018-12-31 16:11:45 +0900

    Add to parse token via query params using react router

--- src/components/Topic/Topic.jsx
+++ src/components/Topic/Topic.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import querystring from 'query-string';
 
 import auth from '../../services/authService';
 
 class Topic extends Component {
   componentDidMount() {
     console.log(this.props.location.search);
-    const params = new URLSearchParams(this.props.location.search);
-    const token = params.get('token');
 
-    if (token) auth.loginWithJwt(token);
+    const values = querystring.parse(this.props.location.search);
+    if (values) auth.loginWithJwt(values.token);
   }
   render() {
     return (

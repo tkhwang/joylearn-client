823ed26f5a22afe67a048d06d5fda17edf776bfa
calmgust <calmgust@gmail.com>
2019-01-02 20:19:35 +0900

    add courses

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -5,21 +5,62 @@ import querystring from 'query-string';
 
 import CoursesTitle from './CoursesTitle';
 import CoursesList from './CoursesList';
-import CoursesFilter from './CoursesFilter';
+// import CoursesFilter from './CoursesFilter';
 
 import './Courses.css';
 import config from '../../config';
 const { SERVER_URL } = config();
 
 class Courses extends Component {
-  state = {};
+  state = {
+    title: {
+      name: 'JavaScript',
+      logo:
+        'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'
+    },
+    courses: [
+      {
+        name: '초심자를 위한 자바스크립트',
+        period: 3
+      },
+      {
+        name: '이것만 따라하면 자바스크립트 완전 정복',
+        period: 4
+      },
+      {
+        name: '뉴비 코더들을 위한 자바스크립트',
+        period: 2
+      },
+      {
+        name: '자바스크립트 고수되기',
+        period: 3
+      },
+      {
+        name: '자바스크립트의 왕도',
+        period: 5
+      },
+      {
+        name: '초보를 위한 자바스크립트',
+        period: 2
+      }
+    ]
+  };
 
   render() {
+    console.log(this.state.courses);
     return (
       <React.Fragment>
-        <CoursesTitle />
-        <CoursesList />
-        <CoursesFilter />
+        <CoursesTitle title={this.state.title} />
+        {this.state.courses.map((course, index) => {
+          return (
+            <CoursesList
+              name={course.name}
+              period={course.period}
+              key={index}
+            />
+          );
+        })}
+        {/* <CoursesFilter /> */}
       </React.Fragment>
     );
   }
--- src/components/Courses/CoursesTitle.jsx
+++ src/components/Courses/CoursesTitle.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
 
+import './CoursesTitle.css';
+
+// className 변경 해야되나?
 class CoursesTitle extends Component {
-  state = {};
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    logo: PropTypes.string.isRequired
+  };
+
   render() {
-    return;
+    return (
+      <div className="topic">
+        <TopicLogo image={this.props.title.logo} />
+        <h1 className="topic-title">{this.props.title.name}</h1>
+        <h3 className="topic-content">{`${
+          this.props.title.name
+        } Tutorials and Courses`}</h3>
+      </div>
+    );
   }
 }
+
+const TopicLogo = ({ image }) => {
+  return <img className="logoimage" src={image} alt="Selected Topic Logo" />;
+};
+
+TopicLogo.propTypes = {
+  image: PropTypes.string.isRequired
+};
 
 export default CoursesTitle;

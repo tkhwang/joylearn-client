5cae4ee6626ea2f52987173725ac559c68424141
tkhwang <tkhwang@gmail.com>
2019-01-04 13:04:47 +0900

    Disable local register menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,17 +4,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavBar = ({ user }) => {
   return (
-    <nav className="navbar navbar-dark py-0 bg-info navbar-expand-lg py-md-0">
-      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light"> */}
-      <Link className="navbar-brand" to="/">
-        J<FontAwesomeIcon icon="grin-alt" />Y
-      </Link>
+    <nav className="navbar navbar-dark justify-content-between py-0 bg-info navbar-expand-lg py-md-0">
       <button
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
+        data-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
@@ -22,9 +18,9 @@ const NavBar = ({ user }) => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          {/* <NavLink className="nav-item nav-link active" to="/instructor">
-            Instructors <span className="sr-only">(current)</span>
-          </NavLink> */}
+          <NavLink className="nav-item nav-link" to="/">
+            J<FontAwesomeIcon icon="grin-alt" />Y
+          </NavLink>
           <NavLink className="nav-item nav-link" to="/topic">
             Topic
           </NavLink>
@@ -45,12 +41,13 @@ const NavBar = ({ user }) => {
               <NavLink className="nav-item nav-link navbar-right" to="/login">
                 Login
               </NavLink>
-              <NavLink
+              {/* TODO:  Disable local login*/}
+              {/* <NavLink
                 className="nav-item nav-link navbar-right"
                 to="/register"
               >
                 Register
-              </NavLink>
+              </NavLink> */}
             </React.Fragment>
           )}
           {user && (
@@ -64,6 +61,22 @@ const NavBar = ({ user }) => {
             </React.Fragment>
           )}
         </div>
+        <span>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-light my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </span>
       </div>
     </nav>
   );

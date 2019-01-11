1a7f4d815dac57be11f66745b9ee19189a9b9e42
tkhwang <tkhwang@gmail.com>
2019-01-11 17:48:30 +0900

    Fix Avatar problem temporarily

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -14,23 +14,14 @@ class Avatar extends Component {
     const user = JSON.parse(localStorage.getItem(KEY_USER));
     this.setState({
       ...this.state,
-      avatar: user.avatar
+      avatar: user ? user.avatar : ''
     });
   }
   render(props) {
     const { width, height } = this.props;
-    return (
-      <img
-        src={
-          this.state.avatar
-            ? this.state.avatar
-            : 'https://s3.ap-northeast-2.amazonaws.com/joy-learn-image/avatar/9205abe37b860fdec767a3123566b573'
-        }
-        alt="avatar"
-        width={width}
-        height={height}
-      />
-    );
+    return this.state.avatar ? (
+      <img src={this.state.avatar} alt="avatar" width={width} height={height} />
+    ) : null;
   }
 }
 

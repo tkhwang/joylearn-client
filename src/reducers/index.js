72ce712d3606d8e938c6a7b3e677409eb9ac0cfe
calmgust <calmgust@gmail.com>
2019-01-14 20:16:13 +0900

    add reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { connectRouter } from 'connected-react-router';
 import signinReducer from './signin';
 import topicsReducer from './topics';
 import instructorReducer from './instructor';
+import bookReducer from './book';
 
 export default history =>
   combineReducers({
     signin: signinReducer,
     topics: topicsReducer,
     instructor: instructorReducer,
+    book: bookReducer,
     router: connectRouter(history)
   });

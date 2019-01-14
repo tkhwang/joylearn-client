21cc37db2ed1f0d56c5153fbf59a2738b99b6f5c
tkhwang <tkhwang@gmail.com>
2019-01-14 18:03:17 +0900

    Add lecture comment db and api

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { connectRouter } from 'connected-react-router';
 import signinReducer from './signin';
 import topicsReducer from './topics';
 import instructorReducer from './instructor';
+import lectureReducer from './lecture';
 
 export default history =>
   combineReducers({
     signin: signinReducer,
     topics: topicsReducer,
     instructor: instructorReducer,
+    lecture: lectureReducer,
     router: connectRouter(history)
   });
